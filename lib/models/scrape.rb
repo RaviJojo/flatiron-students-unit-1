cf289bb5c8ad0e695abc55fdc76c5b83d6641504
github username <github email address>
2013-10-16 14:40:17 -0400

    progress on scraper

--- lib/models/scrape.rb
+++ lib/models/scrape.rb
@@ -1,14 +1,61 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
+require 'awesome_print'
+PATH = "http://students.flatironschool.com/students/vinneycavallo.html"
 # take these out when env is set up.
 
 class StudentSiteScraper
+  attr_accessor :doc
+  # a single student page
 
   def initialize(url)
     @url = url
   end
-  
-  @student_page = Nokogiri::HTML(open(@url))
+
+  def call
+    @doc = Nokogiri::HTML(open(@url))
+  end
+
+  def name
+    @doc.css(".ib_main_header").children.text
+  end
+
+  def twitter
+    @doc.css(".social-icons a")[0].attr('href').value
+  end
+
+  def linkedin
+    @doc.css(".social-icons a")[1].attr('href').value
+  end
+
+  def github
+    @doc.css(".social-icons a")[2].attr('href').value
+  end
+
+  def radar
+    @doc.css(".social-icons a")[3].attr('href').value
+  end
+  # this is the "rss"/facebook link.
+
+  def quote
+    @doc.css(".textwidget").text.strip
+  end
+
+  def biography
+    @doc.css("#ok-text-column-2 .services p").first.content.gsub("\n","")
+  end
+
+  def blogs
+    all_blogs = @doc.css("#ok-text-column-3 .services p")[0].css("a").collect do |item|
+      item.attr('href')
+    end
+  end
+    #=> this returns an array of blogs
+
+
+
+
 
 end
 
@@ -19,3 +66,8 @@ class StudentIndexScraper
   # should probably also get the excerpts and stuff.
 
 end
+
+test = StudentSiteScraper.new(PATH)
+test.call
+
+binding.pry
