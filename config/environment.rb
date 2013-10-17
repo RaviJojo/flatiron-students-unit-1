de2dd3312ecff1ae1f2dd5c8ec8b7e2fb063de34
jnoconor <joe.oconor@gmail.com>
2013-10-17 13:49:21 -0400

    fix fix fix

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require 'erb'
 require 'pry'
 
 URL = "http://students.flatironschool.com"
-require_relative './lib/models/student'
-require_relative './lib/models/scrape'
-require_relative './lib/models/clistudent'
-require_relative './lib/models/site-generator'
+require_relative '../lib/models/student'
+require_relative '../lib/models/scrape'
+require_relative '../lib/models/clistudent'
+require_relative '../lib/models/site-generator'
